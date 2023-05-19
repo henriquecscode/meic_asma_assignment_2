1223be2162356253ce3d517d315938c595d9e5c1
henriquecscode <henriquecostasousa.hcs@gmail.com>
2023-05-19 20:30:19 +0100

    better verbose for learning routines

--- training_utils.py
+++ training_utils.py
@@ -17,17 +17,20 @@ def setup_training_interrupt():
     signal.signal(signal.SIGINT, handler)
 
 
-def create_training_routine(model: BaseAlgorithm, path="", start=0):
+def  create_training_routine(model: BaseAlgorithm, path="", start=0):
+    setup_training_interrupt()
     if path == "":
         path = get_model_path(model, "", "")
 
     def learning_routine(episodes=EPISODES, timesteps=TIMESTEPS):
         # Use a negative number to run forever
         episode_count = 1
+        total_episodes = episodes
         while (episodes != 0 and not stop_training):
             model.learn(timesteps, reset_num_timesteps=False, tb_log_name=path)
             episode_path = f"{path}/{start + timesteps*episode_count}"
             save_model_to(model, episode_path)
+            print(f"Finished {episode_count}/{total_episodes} episodes for {model.__class__.__name__} in {path}")
             episodes -= 1
             episode_count += 1
 
