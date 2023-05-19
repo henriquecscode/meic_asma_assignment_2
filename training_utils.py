96fa23e3608c5aaa9a77fb91f2392868fc53e996
henriquecscode <henriquecostasousa.hcs@gmail.com>
2023-05-19 18:07:12 +0100

    formatting

--- training_utils.py
+++ training_utils.py
@@ -16,22 +16,22 @@ def handler(signum, frame):
 def setup_training_interrupt():
     signal.signal(signal.SIGINT, handler)
 
-def create_training_routine(model: BaseAlgorithm, path="", start = 0):
+
+def create_training_routine(model: BaseAlgorithm, path="", start=0):
     if path == "":
         path = get_model_path(model, "", "")
 
-    def learning_routine(episodes = EPISODES, timesteps = TIMESTEPS):
+    def learning_routine(episodes=EPISODES, timesteps=TIMESTEPS):
         # Use a negative number to run forever
         episode_count = 1
-        while(episodes != 0 and not stop_training):
+        while (episodes != 0 and not stop_training):
             model.learn(timesteps, reset_num_timesteps=False, tb_log_name=path)
             episode_path = f"{path}/{start + timesteps*episode_count}"
             save_model_to(model, episode_path)
             episodes -= 1
             episode_count += 1
 
-        print("Stopping training routine")
-
+        print("Finished training routine")
 
     return learning_routine
 
