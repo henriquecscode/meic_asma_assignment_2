9fc5cdb9cc604ad3fbbd2a59854db4ecd553c9e7
henriquecscode <henriquecostasousa.hcs@gmail.com>
2023-05-23 16:50:27 +0100

    output episode time

--- training_utils.py
+++ training_utils.py
@@ -2,6 +2,7 @@
 from utils import *
 from stable_baselines3.common.base_class import BaseAlgorithm
 import signal
+import datetime
 
 EPISODES = 10
 TIMESTEPS = 1000
@@ -27,10 +28,14 @@ def  create_training_routine(model: BaseAlgorithm, path="", start=0):
         episode_count = 1
         total_episodes = episodes
         while (episodes != 0 and not stop_training):
+            episode_start = datetime.datetime.now()
             model.learn(timesteps, reset_num_timesteps=False, tb_log_name=path)
+            episode_end = datetime.datetime.now()
+            duration = episode_end - episode_start
+            duration = duration - datetime.timedelta(microseconds=duration.microseconds)
             episode_path = f"{path}/{start + timesteps*episode_count}"
             save_model_to(model, episode_path)
-            print(f"Finished {episode_count}/{total_episodes} episodes for {model.__class__.__name__} in {path}")
+            print(f"Finished {episode_count}/{total_episodes} episodes for {model.__class__.__name__} in {duration} in {path}")
             episodes -= 1
             episode_count += 1
 
